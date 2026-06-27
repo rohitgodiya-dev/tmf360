d566c14979da2f3d0c5d28fc25d7ce73ce86104b
Rohit <rohit.godiyaa@gmail.com>
2026-06-26 21:12:53 -0500

    Redirect to static landing.html directly

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
-    window.location.href = "/landing";
+    window.location.href = "/landing.html";
   }, []);
   return null;
 }
