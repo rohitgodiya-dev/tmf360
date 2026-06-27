0e374d25c3f17f32183a98b3bef90481fb564640
Rohit <rohit.godiyaa@gmail.com>
2026-06-26 20:08:20 -0500

    Move app to platform route

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
-    window.location.href = "/app";
+    window.location.href = "/platform";
   }, []);
   return null;
 }
