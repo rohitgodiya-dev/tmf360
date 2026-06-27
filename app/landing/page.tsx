18ee7758767856084788a2272935e95fec037fe0
Rohit <rohit.godiyaa@gmail.com>
2026-06-26 20:01:41 -0500

    Add landing page and app route

---
+++ app/landing/page.tsx
@@ -0,0 +1,14 @@
+export default function LandingPage() {
+  return (
+    <iframe
+      src="/landing.html"
+      style={{
+        width: "100%",
+        height: "100vh",
+        border: "none",
+        display: "block"
+      }}
+      title="TMF360 Landing Page"
+    />
+  );
+}
